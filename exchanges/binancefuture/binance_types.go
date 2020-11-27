2f3e402415bf85814356672e5f0f524868e9355f
Harry <machengxue707@gmail.com>
2020-11-28 06:43:19 +0800

    change the QueryOrderData from spot type to future type

--- exchanges/binancefuture/binance_types.go
+++ exchanges/binancefuture/binance_types.go
@@ -330,25 +330,51 @@ type CancelOrderResponse struct {
 type QueryOrderData struct {
 	Code                int     `json:"code"`
 	Msg                 string  `json:"msg"`
-	Symbol              string  `json:"symbol"`
-	OrderID             int64   `json:"orderId"`
-	ClientOrderID       string  `json:"clientOrderId"`
-	Price               float64 `json:"price,string"`
-	OrigQty             float64 `json:"origQty,string"`
-	ExecutedQty         float64 `json:"executedQty,string"`
-	Status              string  `json:"status"`
-	TimeInForce         string  `json:"timeInForce"`
-	Type                string  `json:"type"`
-	Side                string  `json:"side"`
-	StopPrice           float64 `json:"stopPrice,string"`
-	IcebergQty          float64 `json:"icebergQty,string"`
-	Time                float64 `json:"time"`
-	IsWorking           bool    `json:"isWorking"`
-	CummulativeQuoteQty float64 `json:"cummulativeQuoteQty,string"`
-	OrderListID         int64   `json:"orderListId"`
-	OrigQuoteOrderQty   float64 `json:"origQuoteOrderQty,string"`
-	UpdateTime          int64   `json:"updateTime"`
+	// Symbol              string  `json:"symbol"`
+	// OrderID             int64   `json:"orderId"`
+	// ClientOrderID       string  `json:"clientOrderId"`
+	// Price               float64 `json:"price,string"`
+	// OrigQty             float64 `json:"origQty,string"`
+	// ExecutedQty         float64 `json:"executedQty,string"`
+	// Status              string  `json:"status"`
+	// TimeInForce         string  `json:"timeInForce"`
+	// Type                string  `json:"type"`
+	// Side                string  `json:"side"`
+	// StopPrice           float64 `json:"stopPrice,string"`
+	// IcebergQty          float64 `json:"icebergQty,string"`
+	// Time                float64 `json:"time"`
+	// IsWorking           bool    `json:"isWorking"`
+	// CummulativeQuoteQty float64 `json:"cummulativeQuoteQty,string"`
+	// OrderListID         int64   `json:"orderListId"`
+	// OrigQuoteOrderQty   float64 `json:"origQuoteOrderQty,string"`
+	// UpdateTime          int64   `json:"updateTime"`
+	//===== future =======
+	AvgPrice            string  `json:"avgPrice"`
+    ClientOrderId       string  `json:"clientOrderId"`
+    CumQuote            string  `json:"cumQuote"`
+    ExecutedQty         string  `json:"executedQty"`
+    OrderId             int64   `json:"orderId"`
+    OrigQty             string  `json:"origQty"`
+    OrigType            string  `json:"origType"`
+    Price               string  `json:"price"`
+    ReduceOnly          bool    `json:"reduceOnly"`
+    Side                string  `json:"side"`
+    PositionSide        string  `json:"positionSide"`
+    Status              string  `json:"status"`
+    StopPrice           string  `json:"stopPrice"`
+    ClosePosition       bool    `json:"closePosition"`
+    Symbol              string  `json:"symbol"`
+    Time                int64   `json:"time"`
+    TimeInForce         string  `json:"timeInForce"`
+    Type                string  `json:"type"`
+    ActivatePrice       string  `json:"activatePrice"`
+    PriceRate           string  `json:"priceRate"`
+    UpdateTime          int64  	`json:"updateTime"`
+    WorkingType         string  `json:"workingType"`
+    PriceProtect        bool  	`json:"priceProtect"`
 }
+
+//
 
 // Balance holds query order data
 type Balance struct {
